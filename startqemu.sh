44ef3e7a8662a2bf9008089a74237f7de7d66673
belajarit45 <144545476+belajarit45@users.noreply.github.com>
2024-07-01 09:55:42 +0700

    Update startqemu.sh

--- startqemu.sh
+++ startqemu.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-qemu-system-x86_64 -machine q35 -m 1024 -smp cpus=2 -cpu qemu64 \
+qemu-system-x86_64 -machine q35 -m 2048 -smp cpus=4 -cpu qemu64 \
   -drive if=pflash,format=raw,read-only=on,file=$PREFIX/share/qemu/edk2-x86_64-code.fd \
-  -netdev user,id=n1,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 \
+  -netdev user,id=n1,hostfwd=tcp::2222-:22,hostfwd=tcp::9000-:9000 -device virtio-net,netdev=n1 \
   -nographic alpine.img
